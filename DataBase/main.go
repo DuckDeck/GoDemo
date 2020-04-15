1e5ff01afe722ca5251aa9724808d3ed9a00e3a3
DuckDeck <3421902@qq.com>
2020-04-15 15:55:44 +0800

    process code

--- DataBase/main.go
+++ DataBase/main.go
@@ -31,7 +31,7 @@ func main() {
 		fmt.Println("init DB fail err:", err)
 		return
 	}
-	query()
+	transaction()
 	defer db.Close()
 }
 
@@ -108,3 +108,49 @@ func update() {
 }
 
 //delete就不需要写了
+//mysql
+func prepareInsert() {
+	sql := `insert into five_code(word,pin_yin,five_code) values(?,?,?)`
+	stmt, err := db.Prepare(sql)
+	if err != nil {
+		fmt.Printf("sql: %s prepare fail :%a\n", sql, err)
+		return
+	}
+	defer stmt.Close()
+	//后续只要用到stmp
+	res, _ := stmt.Exec("小", "xiao", "ih")
+	id, _ := res.LastInsertId()
+	fmt.Println("inser success thr id is ", id)
+}
+
+//事务
+func transaction() {
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println("begin transcation fail err:", err)
+		return
+	}
+	sql1 := `update five_code set img_keyboard = 'asdfadfs' where id = 1`
+	sql2 := `update five_code set img_keyboard = 'zzzzzzzzzzzzzzz' where id = 2`
+	_, err = tx.Exec(sql1)
+	if err != nil {
+		tx.Rollback()
+		fmt.Println("执行sql1出错了，回滚,err:", err)
+		return
+	}
+	tx.Exec(sql2)
+	_, err = tx.Exec(sql2)
+	if err != nil {
+		tx.Rollback()
+		fmt.Println("执行sql2出错了，回滚,err:", err)
+		return
+	}
+	//上面两次事务执行成功，可以提交
+	err = tx.Commit()
+	if err != nil {
+		fmt.Println("事务提交出错回滚 err:", err)
+		return
+	}
+	fmt.Println("事务执行成功")
+
+}
