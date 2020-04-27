64739dbfbe29feb28cbf404e343f647d688d8ba9
DuckDeck <3421902@qq.com>
2020-04-27 20:12:51 +0800

    process code

--- Project/Mall/main.go
+++ Project/Mall/main.go
@@ -1,20 +1,49 @@
 package main
 
 import (
+	"database/sql"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 )
+
+type User struct {
+	gorm.Model
+	Name         string
+	Age          sql.NullInt64
+	Birthday     *time.Time
+	Email        string  `gorm:"type:varchar(120);unique_index"`
+	Role         string  `gorm:"size:255"`
+	MemberNumber *string `gorm:"unique;not null"`
+	Num          int     `gorm:"AUTO_INCREMENT"`
+	Address      string  `gorm:"index:addr"`
+	IgnoreMe     int     `gorm:"-"` //无视这个
+}
 
 func a(c *gin.Context) {
 	c.JSON(200, gin.H{"message": "hello you"})
 }
-
-func main() {
+func startServer() {
 	r := gin.Default()
 	r.GET("/hello", a)
-	r.POST("/b",func(c * gin.Context){
-		c.JSON(200.gin.H{
-			"method":"POST"
+	r.POST("/b", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"method": "POST",
 		})
 	})
 	r.Run(":8889")
+}
+func initDB() {
+	db, err := gorm.Open("mysql", "root:Stanhu520.@tcp(127.0.0.1:3306)/mall")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	db.AutoMigrate(&User{})
+}
+func main() {
+	//startServer()
+	initDB()
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gin-gonic/gin v1.6.2 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gofrs/uuid v3.2.0+incompatible // indirect
+	github.com/jinzhu/gorm v1.9.12 // indirect
 	github.com/jmoiron/sqlx v1.2.0 // indirect
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/satori/go.uuid v1.2.0
