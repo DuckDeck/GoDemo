51f16ba2d4671afb123daa72ad7ebc4373382a02
DuckDeck <3421902@qq.com>
2020-06-17 15:27:32 +0800

    upload file ok

--- Project/FileHandle/main.go
+++ Project/FileHandle/main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"mime/multipart"
-	"os"
+	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	// "path"
 	// "path/filepath"
-	// "strconv"
 	// "strings"
 	// "github.com/PuerkitoBio/goquery"
 	// _ "github.com/go-sql-driver/mysql"
@@ -23,35 +22,35 @@ var (
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
+	r.StaticFS("/", http.Dir("./img"))
 	r.POST("/upload", uploadHandler)
-	r.Run(":9090")
+	r.Run("0.0.0.0:9090")
 }
 
 func uploadHandler(c *gin.Context) {
 	header, err := c.FormFile(uploadFileKey)
-	fmt.Println(header)
 	if err != nil {
-		//ignore
+		fmt.Println(err)
+		c.JSON(200, gin.H{
+			"code": -11,
+			"msg":  "request not contail file",
+		})
+		return
 	}
-	dst := header.Filename
-	// gin 简单做了封装,拷贝了文件流
-	if err := c.SaveUploadedFile(header, dst); err != nil {
 
+	dst := strconv.FormatInt(time.Now().Unix(), 10) + header.Filename
+	// gin 简单做了封装,拷贝了文件流
+	if err := c.SaveUploadedFile(header, "img/"+dst); err != nil {
+		fmt.Println(err)
+		c.JSON(200, gin.H{
+			"code": -10,
+			"msg":  "save file fail",
+		})
+	} else {
+		c.JSON(200, gin.H{
+			"code": 200,
+			"msg":  "success",
+			"data": "http://lovelive.ink:10087/" + dst,
+		})
 	}
-}
-func SaveUploadedFile(file *multipart.FileHeader, dst string) error {
-	src, err := file.Open()
-	if err != nil {
-		return err
-	}
-	defer src.Close()
-	//创建 dst 文件
-	out, err := os.Create(dst)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-	// 拷贝文件
-	_, err = io.Copy(out, src)
-	return err
 }
